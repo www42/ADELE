7f92deac6f92b84ba5b176691d0e50bbc74ec45a
Thomas Jaekel <Willi.Wien@outlook.de>
2017-11-20 20:47:54 +0100

    profile template

---
+++ RUN_FIRST_ON_NEW_HOST.ps1
@@ -0,0 +1,26 @@
+﻿# ---------------------------------------------
+# Clone ADELE repository to local path $GitPath
+# ---------------------------------------------
+
+$GitPath = "C:\Git\ADELE"
+
+# Copy PowerShell profile template
+New-Item -ItemType File -Path $profile.CurrentUserAllHosts
+copy $GitPath\profile.ps1 $profile.CurrentUserAllHosts
+
+# Install NuGet
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Force
+
+# Register PSRepository
+$RepoName = "MyGet"
+$SourceLocation  = 'https://www.myget.org/F/tj/api/v2'
+$PublishLocation = 'https://www.myget.org/F/tj/api/v2/package/'
+ 
+Register-PSRepository -Name $RepoName `
+                      -SourceLocation $SourceLocation `
+                      -PublishLocation $PublishLocation `
+                      -InstallationPolicy Trusted `
+                      -PackageManagementProvider "NuGet"
+
+# Install Modules
+Install-Module -Name ADELE,tjLabs,tjTools -Repository $RepoName
---
+++ profile.ps1
@@ -0,0 +1,42 @@
+#  $profile.CurrentUserAllHosts
+
+[string]$Lab                = "PWS"
+[string]$LabDir             = "C:\Labs\PWS"
+[string]$LabSwitch          = "PWS"
+[string]$LabBaseGen1        = "C:\Base\vyos-999.201703132237-amd64.vhd"
+[string]$LabBaseGen2        = "C:\Base\Base-WS2016_DesktopExperience_withUpdates1710_en-US.vhdx"
+[long]  $LabVmMem           = 2GB
+[long]  $LabVmCpuCount      = 2
+[string]$LabVmVersion       = "8.0"
+[string]$LabIpSubnet        = "10.80.0.0"
+[string]$LabIpPrefixLength  = "16"
+[string]$LabIpDefaultGw     = "10.80.0.1"
+[string]$LabIpDnsServer     = "10.80.0.10"
+[string]$LabAdDomain        = "Adatum.com"
+[string]$LabAdDomainNetBios = "ADATUM"
+[string]$LabPw              = 'Pa55w.rd'
+
+
+# [string]$Lab                = "10961C"
+# [string]$LabDir             = "C:\Program Files\Microsoft Learning\10961\Drives"
+# [string]$LabSwitch          = "Private Network"
+# [string]$LabIpSubnet        = "172.16.0.0"
+# [string]$LabIpPrefixLength  = "16"
+# [string]$LabIpDefaultGw     = "172.16.0.1"
+# [string]$LabIpDnsServer     = "172.16.0.10"
+
+function prompt {"$Lab> "}
+
+Import-Module -Name "ADELE"
+Import-Module -Name "tjLabs" -WarningAction SilentlyContinue
+Import-Module -Name "tjTools"
+
+Write-Output "Loading Modules ..."
+Get-Module    -Name ADELE,tjLabs,tjTools | ft Name,Version
+
+$DomCred = New-Object -TypeName System.Management.Automation.PSCredential 'Adatum\Administrator',(ConvertTo-SecureString $LabPw -AsPlainText -Force)
+$LocalCred = New-Object -TypeName System.Management.Automation.PSCredential 'Administrator',(ConvertTo-SecureString $LabPw -AsPlainText -Force)
+
+if (Test-Path $LabDir) {cd $LabDir}
+Write-Output "Current Lab is $Lab."
+Show-Lab
