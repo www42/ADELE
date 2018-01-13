418486a75f33ad0e27025ac33b47b2efa72cbe10
Thomas Jaekel <Wilhelm.Wien@outlook.de>
2018-01-13 11:26:47 +0100

    Minor changes

--- RUN_FIRST_ON_NEW_HOST.ps1
+++ RUN_FIRST_ON_NEW_HOST.ps1
@@ -1,17 +1,43 @@
-﻿# ---------------------------------------------
-# Clone ADELE repository to local path $GitPath
-# ---------------------------------------------
+﻿#region Install Git
 
-$GitPath = "C:\Git\ADELE"
+Start-Process "https://git-scm.com/"
+# ---------------------
+# Install git manually
+# ---------------------
 
-# Copy PowerShell profile template
+mkdir C:\Git
+cd C:\Git\
+
+git config --global user.name "Thomas Jaekel"
+git config --global user.email "Wilhelm.Wien@outlook.de"
+#git config --global http.proxy http://192.168.254.5:8080 
+#git config --global --unset http.proxy 
+git config --list 
+
+#endregion
+
+#region Clone ADELE repo
+
+git clone https://github.com/www42/ADELE.git
+
+cd .\ADELE\
+git status
+
+#endregion
+
+#region Create PowerShell profile
+
+$AdelePath = "C:\Git\ADELE"
+
 New-Item -ItemType File -Path $profile.CurrentUserAllHosts
-copy $GitPath\profile.ps1 $profile.CurrentUserAllHosts
+copy $AdelePath\profile.ps1 $profile.CurrentUserAllHosts
 
-# Install NuGet
+#endregion
+
+#region  Install PowerShell modules
+
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.208 -Force
 
-# Register PSRepository
 $RepoName = "MyGet"
 $SourceLocation  = 'https://www.myget.org/F/tj/api/v2'
 $PublishLocation = 'https://www.myget.org/F/tj/api/v2/package/'
@@ -22,5 +48,6 @@ Register-PSRepository -Name $RepoName `
                       -InstallationPolicy Trusted `
                       -PackageManagementProvider "NuGet"
 
-# Install Modules
 Install-Module -Name ADELE,tjLabs,tjTools -Repository $RepoName
+
+#endregion
